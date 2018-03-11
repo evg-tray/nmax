3aead3864735a9c038ca940d1af9d11b2b10121c
Evg <evg.tray@gmail.com>
2018-03-11 19:21:35 +0300

    Add numbers finder

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,5 +1,48 @@
 require "nmax/version"
 
 module Nmax
-  # Your code goes here...
+  CHUNK_SIZE = 1_000_000
+  MAX_DIGITS = 1000
+
+  def self.find_numbers(stream, numbers_count)
+    return if stream.tty? || numbers_count < 1
+
+    result = {}
+    last_number = ''
+
+    while chunk = stream.read(CHUNK_SIZE)
+      chunk = last_number + chunk
+      new_last_number = chunk.match(/\d+\z/).to_s
+
+      end_size = chunk.size - new_last_number.size - 1
+      if end_size > 0
+        chunk[0..end_size].scan(/\d+/).uniq.each do |n|
+          insert_number(result, n, numbers_count)
+        end
+      end
+
+      last_number = new_last_number
+    end
+
+    insert_number(result, last_number, numbers_count) if last_number != ''
+
+    result.map { |k,v| k }.max(numbers_count)
+  end
+
+  private
+
+  def self.insert_number(result, number, numbers_count)
+    return if number.size > MAX_DIGITS
+
+    num = number.to_i
+    if result.size == numbers_count
+      min = result.min.first
+      if num > min
+        result.delete(min)
+        result[num] = nil
+      end
+    else
+      result[num] = nil
+    end
+  end
 end
--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -3,7 +3,89 @@ RSpec.describe Nmax do
     expect(Nmax::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "#find_numbers" do
+    let(:numbers_count) { 3 }
+    let(:stream) { StringIO.new("text data") }
+
+    subject { described_class.find_numbers(stream, numbers_count) }
+
+    context "when valid params given" do
+
+      context "when input has no numbers" do
+        it "should return empty array" do
+          expect(subject).to eq []
+        end
+      end
+
+      context "when input has numbers" do
+        let(:number1) { rand(1000) }
+        let(:number2) { rand(1000) }
+        let(:number3) { rand(1000) }
+        let(:number4) { rand(1000) }
+        let(:big_number) { "1#{'0'*1000}".to_i + 1 }
+
+        let(:stream) { StringIO.new(text_data) }
+
+        context "when input has no numbers more than 1000 digits" do
+          let(:text_data) { "#{number1}text#{number2}text#{number3}text#{number4}" }
+
+          it "should return 3 maximum numbers" do
+            result = [number1, number2, number3, number4].max(3)
+            expect(subject).to eq result
+          end
+        end
+
+        context "when input has numbers with more than 1000 digits" do
+          let(:text_data) { "#{number1}text#{number2}text#{big_number}" }
+
+          it "should not return numbers with more than 1000 digits" do
+            expect(subject).not_to include big_number
+          end
+        end
+
+        context "when number between reading chunks" do
+          let(:number) { "#{rand(100)}#{'1'*10}".to_i }
+          let(:text_data) { "text text #{number} text text"}
+          let(:stream) { StringIO.new(text_data) }
+
+          before { stub_const("Nmax::CHUNK_SIZE", 4) }
+
+          it "should return this number" do
+            expect(subject).to eq [number]
+          end
+        end
+      end
+    end
+
+    context "when invalid stream param given" do
+      let(:stream) { StringIO.new }
+
+      before { allow_any_instance_of(StringIO).to receive(:tty?).and_return(true) }
+
+      it "should return nil" do
+        expect(subject).to eq nil
+      end
+    end
+
+    context "when invalid number_count param given" do
+      let(:numbers_count) { 0 }
+
+      it "should return nil" do
+        expect(subject).to eq nil
+      end
+    end
+
+    context "when invalid stream and number_count params given" do
+      let(:stream) { StringIO.new }
+      let(:numbers_count) { 0 }
+
+      before { allow_any_instance_of(StringIO).to receive(:tty?).and_return(true) }
+
+      it "should return nil" do
+        expect(subject).to eq nil
+      end
+    end
+
   end
+
 end
